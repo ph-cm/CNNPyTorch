cfd66b73dccfd3ce840a29a7280f8c6bdb0acb28
Pedro Henrique <trabalho.prog.pedro@gmail.com>
2024-11-25 00:11:35 -0300

    Multi-layered CNNs and pooling layers

--- CNNTorch.py
+++ CNNTorch.py
@@ -166,3 +166,24 @@ with torch.no_grad():
         ax[i].imshow(x.detach().cpu()[0,...])
         ax[i].axis('off')
     plt.show()
+
+#Multi-layered CNNs and pooling layers
+class MultiLayerCNN(nn.Module):
+    def __init__(self):
+        super(MultiLayerCNN, self).__init__()
+        self.conv1 = nn.Conv2d(1, 10, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(10, 20, 5)
+        self.fc = nn.Linear(320,10)
+
+    def forward(self, x):
+        x = self.pool(nn.functional.relu(self.conv1(x)))
+        x = self.pool(nn.functional.relu(self.conv2(x)))
+        x = x.view(-1, 320)
+        x = nn.functional.log_softmax(self.fc(x),dim=1)
+        return x
+
+net = MultiLayerCNN()
+summary(net,input_size=(1,1,28,28))
+
+history = train(net,train_loader, test_loader,epoch=5)
